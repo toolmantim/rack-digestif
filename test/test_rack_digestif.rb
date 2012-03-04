b47537c0763f584fbdcdbb9278e76c0c29e13424
Tim Lucas <t.lucas@toolmantim.com>
2012-03-04 14:41:35 +0100

    Simplify the tests

--- test/test_rack_digestif.rb
+++ test/test_rack_digestif.rb
@@ -10,24 +10,20 @@ class TestRackDigestif < Test::Unit::TestCase
   def app_with_digestif path=nil
     Rack::Digestif.new(app, path)
   end
-  def assert_rewrites source_path, rewritten_path
+  def assert_rewrite expected_path, source_path
     req = Rack::MockRequest.new(@app)
-    assert_equal rewritten_path, req.get(source_path).body
-  end
-  def assert_not_rewrites path
-    req = Rack::MockRequest.new(@app)
-    assert_equal path, req.get(path).body
+    assert_equal expected_path, req.get(source_path).body
   end
 
   def test_rewrites_path
     @app = app_with_digestif
-    assert_rewrites "/path/f-c20ad4d76fe97759aa27a0c99bff6710.css", "/path/f.css"
-    assert_rewrites "/sub/dir/f-c20ad4d76fe97759aa27a0c99bff6710.css", "/sub/dir/f.css"
-    assert_not_rewrites "/assets/f.css"
+    assert_rewrite "/path/f.css", "/path/f-c20ad4d76fe97759aa27a0c99bff6710.css"
+    assert_rewrite "/sub/dir/f.css", "/sub/dir/f-c20ad4d76fe97759aa27a0c99bff6710.css"
+    assert_rewrite "/assets/f.css", "/assets/f.css"
   end
   def test_rewrites_only_matching_paths
     @app = app_with_digestif "/path"
-    assert_rewrites "/path/f-c20ad4d76fe97759aa27a0c99bff6710.css", "/path/f.css"
-    assert_not_rewrites "/not-path/f-c20ad4d76fe97759aa27a0c99bff6710.css"
+    assert_rewrite "/path/f.css", "/path/f-c20ad4d76fe97759aa27a0c99bff6710.css"
+    assert_rewrite "/not-path/f-c20ad4d76fe97759aa27a0c99bff6710.css", "/not-path/f-c20ad4d76fe97759aa27a0c99bff6710.css"
   end
 end
